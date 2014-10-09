86ceb023729ecddc76710b1e8a52772d1d636058
Marcel Augsburger <maugsburger@gmail.com>
2014-10-09 13:32:15 -0300

    Added more tests for new edit and create

--- spec/controllers/sandboxes_controller_spec.rb
+++ spec/controllers/sandboxes_controller_spec.rb
@@ -24,26 +24,59 @@ RSpec.describe SandboxesController, :type => :controller do
     end
   end
 
+  describe "GET new" do
+    it "assigns a new sandbox as @sandbox" do
+      get :new, {}
+      expect(assigns(:sandbox)).to be_a_new(Sandbox)
+    end
+    it "renders the #new view" do
+      get :new
+      response.should render_template :new
+    end
+  end
+
+  describe "GET edit" do
+    it "assigns the requested sandbox as @sandbox" do
+      sandbox = FactoryGirl.create(:sandbox)
+      get :edit, {:id => sandbox.to_param}
+      expect(assigns(:sandbox)).to eq(sandbox)
+    end
+    it "renders the #edit view" do
+      get :edit, id: FactoryGirl.create(:sandbox)
+      response.should render_template :edit
+    end
+  end
+
   describe "POST create" do
     context "with valid attributes" do
       it "creates a new sandbox" do
-        expect{ post :create, sandbox: FactoryGirl.attributes_for(:sandbox) }.to change(Sandbox,:count).by(1)
+        expect{
+          post :create, sandbox: FactoryGirl.attributes_for(:sandbox)
+        }.to change(Sandbox,:count).by(1)
       end
       it "redirects to the new sandbox" do
         post :create, sandbox: FactoryGirl.attributes_for(:sandbox)
         response.should redirect_to Sandbox.last
       end
+      it "assigns a newly created sandbox as @sandbox" do
+        post :create, {:sandbox => FactoryGirl.attributes_for(:sandbox)}
+        expect(assigns(:sandbox)).to be_a(Sandbox)
+        expect(assigns(:sandbox)).to be_persisted
+      end
     end
 
     context "with invalid attributes" do
+      it "assigns a newly created but unsaved sandbox as @sandbox" do
+        post :create, {:sandbox => FactoryGirl.attributes_for(:invalid_sandbox)}
+        expect(assigns(:sandbox)).to be_a_new(Sandbox)
+      end
       it "does not save the new sandbox" do
         expect{ post :create, sandbox: FactoryGirl.attributes_for(:invalid_sandbox) }.to_not change(Sandbox,:count)
       end
-      it "re-renders the new method" do
+      it "re-renders the 'new' template" do
         post :create, sandbox: FactoryGirl.attributes_for(:invalid_sandbox)
         response.should render_template :new
       end
     end
-
   end
 end
