34ff920b8f6ba38d2b82eecaf1e002816bab30bb
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-10-21 17:55:14 -0300

    added readme to other_name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013223623) do
+ActiveRecord::Schema.define(version: 20141021203710) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "other_names", force: true do |t|
+    t.string   "name"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string   "note"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sandboxes", force: true do |t|
     t.string   "name"
