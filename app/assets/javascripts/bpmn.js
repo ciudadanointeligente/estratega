a2d4156f67aff0fa8f3f1e364a53d03d13402a8e
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-10-28 11:01:47 -0300

    small changes

--- app/assets/javascripts/bpmn.js
+++ app/assets/javascripts/bpmn.js
@@ -692,28 +692,39 @@ joint.shapes.bpmn.Person = joint.dia.Element.extend({
     defaults: joint.util.deepSupplement({
 
         type: 'bpmn.Person',
-        size: { width: 33, height: 33 },
+        size: { 
+            width: 33, 
+            height: 33 
+        },
         attrs: {
             '.body': {
                 fill: '#ffffff',
                 stroke: '#0091EA'
             },
             '.outer': {
-                'stroke-width': 1, r:20,
+                'stroke-width': 1, 
+                r:20,
                 transform: 'translate(30,30)'
             },
             '.inner': {
-                'stroke-width': 0, r: 16,
+                'stroke-width': 0, 
+                r: 16,
                 transform: 'translate(30,30)'
             },
             image: {
-                width:  20, height: 20, 'xlink:href': '', transform: 'translate(20,20)'
+                width:  20, 
+                height: 20, 
+                'xlink:href': '', 
+                transform: 'translate(20,20)'
             },
             '.label': {
                 text: '',
                 fill: '#000000',
-                ref: '.outer', 'ref-x': .5, 'ref-dy': 20,
-                'x-alignment': 'middle', 'y-alignment': 'middle'
+                ref: '.outer', 
+                'ref-x': .5, 
+                'ref-dy': 20,
+                'x-alignment': 'middle', 
+                'y-alignment': 'middle'
             }
         },
         eventType: "start",
@@ -782,13 +793,13 @@ joint.shapes.bpmn.Person = joint.dia.Element.extend({
     setImage: function() {
         var main_id = this.id,
             elem_image = '[model-id='+main_id+'] image',
-            the_image = this.attributes.image;
+            the_image = this.get('image');
 
+        this.set('image',the_image);
         $(elem_image).attr('width',40);
         $(elem_image).attr('height',40);
         $(elem_image).attr('transform','translate(10,10)');
         $(elem_image).attr('href',the_image);
-        
     }
 
 }).extend(joint.shapes.bpmn.IconInterface);
