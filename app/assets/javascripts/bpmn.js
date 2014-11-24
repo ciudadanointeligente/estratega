48f7c783a98b159b81f06b46a05d2df2746bbb2a
Catalina Margozzini <ca.margozzini@gmail.com>
2014-11-24 10:35:08 -0300

    Alerta 'saved'

--- app/assets/javascripts/bpmn.js
+++ app/assets/javascripts/bpmn.js
@@ -2039,10 +2039,10 @@ var toolbar = {
             data: JSON.stringify({sandbox: {graph_data: JSON.stringify(graph.toJSON())}}),
             beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
             success: function(data) {
-                $('#alert').html('Saved').fadeIn().delay(1000).fadeOut();
+                $('.alert').removeClass('alertError').addClass('alertSuccess').html('Saved').fadeIn().delay(1000).fadeOut();
             },
             error: function(data) {
-                $('#alert').html('Not Saved').fadeIn().delay(1000).fadeOut();
+                $('.alert').removeClass('alertSuccess').addClass('alertError').html('Not Saved').fadeIn().delay(1000).fadeOut();
             }
         });
     },
