9104a32e6511e71f9f9395d27bede4a200a57e3a
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2015-02-18 10:36:23 -0300

    create test

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,3 +1,31 @@
-QUnit.test( "hello test", function( assert ) {
-assert.ok( 1 == "1", "Passed!" );
-});
+toolbar.saveGraph = function(){
+	// assert.ok(true)
+}
+
+QUnit.test('Guardar al hacer drag', function( assert) {
+
+	toolbar.saveGraph = function(){
+		assert.ok(true)
+	}
+
+	cell = new joint.shapes.bpmn.Step()
+	collection = {}
+	opt = {}
+
+	graph.trigger('add', cell, collection, opt)
+})
+
+QUnit.test('no linkear un steplink en caso de no tener un destino', function( assert) {
+	var step = new joint.shapes.bpmn.Step()
+	step.paper = paper
+	var person  = new joint.shapes.bpmn.Person()
+	person.paper = paper
+	var link = new joint.shapes.bpmn.StepLink({ source: step })
+		// paper.addCell(step)
+		// paper.addCell(person)
+
+	var halo = new joint.ui.Halo({ graph: graph, cellView: step });
+	halo.startLinking({ })
+	assert.ok(false)
+	console.log(halo)
+})
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,5 @@
 class TestsController < ApplicationController
     def index
+    	render :layout => "sandboxes_test"
     end
 end
