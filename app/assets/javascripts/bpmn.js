345069da208bb2aad9b03bb6eb81d945326dc511
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2015-03-06 12:07:09 -0300

    fix disappear button when click on edit

--- app/assets/javascripts/bpmn.js
+++ app/assets/javascripts/bpmn.js
@@ -1693,8 +1693,10 @@ function openIHF(cellView, edit){
             $("#inspector-container").css('display','none');
         });
 
-    if (edit)
+    if (edit){
         $('#inspector-container').html(inspector.render().el);
+        inspector.$el.prepend(btn_close);
+    }
 
     else {
         if ( function(){
