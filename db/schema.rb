a9688e2a5367b993e560dc47b3ddce41a766325e
José Damián Garrido <damian.garrido.munoz@gmail.com>
2015-03-20 12:58:09 -0300

    add model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320131606) do
+ActiveRecord::Schema.define(version: 20150320144758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 20150320131606) do
   end
 
   create_table "resources", force: true do |t|
-    t.text     "description"
-    t.boolean  "public"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
