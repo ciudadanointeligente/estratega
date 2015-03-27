d4778d122be95889374f4d059d5013b514afd804
Marcel Augsburger <maugsburger@gmail.com>
2015-03-27 15:58:02 -0300

    added to_graph_json method to RealProblem for creating graphs

--- app/models/real_problem.rb
+++ app/models/real_problem.rb
@@ -1,4 +1,9 @@
 class RealProblem < ActiveRecord::Base
   validates :title, presence: true
   has_many :policy_problems, dependent: :destroy
+
+  def to_graph_json 
+    json = { "company" => { "name" => self.title, "styleprop" => { "activity_rounded" => true } } }.to_json
+    return json
+  end
 end
--- spec/models/real_problem_spec.rb
+++ spec/models/real_problem_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe RealProblem, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "returns a RealProblem in json format" do
+    problem = RealProblem.new
+    problem.title = "a real world problem" 
+    json = { "company" => { "name" => "a real world problem", "styleprop" => { "activity_rounded" => true } } }.to_json
+
+    expect(problem.to_graph_json).to eq(json)
+  end
 end
