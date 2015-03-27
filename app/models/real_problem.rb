5921c7b494acc7669a52967cdfd913e597df4bb1
Marcel Augsburger <maugsburger@gmail.com>
2015-03-27 17:54:02 -0300

    Added inner fields to RealProblem.to_graph_json

--- app/models/real_problem.rb
+++ app/models/real_problem.rb
@@ -3,7 +3,42 @@ class RealProblem < ActiveRecord::Base
   has_many :policy_problems, dependent: :destroy
 
   def to_graph_json 
-    json = { "company" => { "name" => self.title, "styleprop" => { "activity_rounded" => true } } }.to_json
+    policy_problem_list = []
+    policy_problems.each do |policy_problem|
+      policy_solution_list = []
+      policy_problem.policy_solutions.each do |policy_solution|
+        policy_solution_hash =
+        {
+          "team" => [
+              {
+                  "head" => {
+                      "name" => policy_solution.title,
+                      "styleprop" => {}
+                  }
+              }
+          ]
+        }
+        policy_solution_list.append policy_solution_hash
+      end
+      policy_problem_hash =
+        {
+          "title" => {
+            "name" => policy_problem.title,
+            "styleprop" => {
+              "activity_rounded" => true
+            }
+          },
+          "subitems" => {
+            "teams" => policy_solution_list
+          }
+        }
+      policy_problem_list.append policy_problem_hash
+    end
+    
+    
+    json = { "company" => { "name" => self.title, "styleprop" => { "activity_rounded" => true } },
+        "projects" => policy_problem_list
+      }.to_json
     return json
   end
 end
--- spec/models/real_problem_spec.rb
+++ spec/models/real_problem_spec.rb
@@ -1,11 +1,119 @@
 require 'rails_helper'
 
 RSpec.describe RealProblem, :type => :model do
-  it "returns a RealProblem in json format" do
-    problem = RealProblem.new
-    problem.title = "a real world problem" 
-    json = { "company" => { "name" => "a real world problem", "styleprop" => { "activity_rounded" => true } } }.to_json
 
-    expect(problem.to_graph_json).to eq(json)
+  describe "to_graph_json" do
+    it "returns a RealProblem in json format" do
+      problem = RealProblem.new
+      problem.title = "a real world problem" 
+      json = { "company" => { "name" => "a real world problem", "styleprop" => { "activity_rounded" => true } }, "projects" => [] }.to_json
+      expect(problem.to_graph_json).to eq(json)
+    end
+
+    it "returns embedded policy problems in the json" do
+      real_problem = RealProblem.new
+      real_problem.title = "a real world problem"
+      policy_problem_1 = PolicyProblem.new
+      policy_problem_1.title = "a policy problem"
+      policy_problem_2 = PolicyProblem.new
+      policy_problem_2.title = "another policy problem"
+      real_problem.policy_problems = [policy_problem_1, policy_problem_2]
+      json = { "company" => { "name" => "a real world problem", "styleprop" => { "activity_rounded" => true } },
+        "projects" => [
+          {
+            "title" => {
+                "name" => "a policy problem",
+                "styleprop" => {
+                  "activity_rounded" => true
+                }
+            },
+            "subitems" => {
+                "teams" => []
+            }
+          },
+          {
+            "title" => {
+              "name" => "another policy problem",
+              "styleprop" => {
+                "activity_rounded" => true
+              }
+            },
+            "subitems" => {
+                "teams" => []
+            }
+          }
+        ]
+      }.to_json
+      expect(real_problem.to_graph_json).to eq(json)
+    end
+
+    it "returns embedded policy solutions in the json" do
+      real_problem = RealProblem.new
+      real_problem.title = "a real world problem"
+      policy_problem_1 = PolicyProblem.new
+      policy_problem_1.title = "a policy problem"
+      policy_problem_2 = PolicyProblem.new
+      policy_problem_2.title = "another policy problem"
+      real_problem.policy_problems = [policy_problem_1, policy_problem_2]
+      policy_solution_1 = PolicySolution.new
+      policy_solution_1.title = "a solution for policy problem 1"
+      policy_solution_2 = PolicySolution.new
+      policy_solution_2.title = "another solution for policy problem 1"
+      policy_problem_1.policy_solutions = [policy_solution_1, policy_solution_2]
+      policy_problem_2.policy_solutions = []
+
+      json = { "company" => { "name" => "a real world problem", "styleprop" => { "activity_rounded" => true } },
+        "projects" => [
+          {
+            "title" => {
+                "name" => "a policy problem",
+                "styleprop" => {
+                  "activity_rounded" => true
+                }
+            },
+            "subitems" => {
+                "teams" => [
+                    {
+                        "team" => [
+                            {
+                                "head" => {
+                                    "name" => "a solution for policy problem 1",
+                                    "styleprop" => {}
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "team" => [
+                            {
+                                "head" => {
+                                    "name" => "another solution for policy problem 1",
+                                    "styleprop" => {}
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+          },
+          {
+            "title" => {
+                "name" => "another policy problem",
+                "styleprop" => {
+                  "activity_rounded" => true
+                }
+            },
+            "subitems" => {
+                "teams" => []
+            }
+          }
+        ]
+      }.to_json
+
+      expect(real_problem.to_graph_json).to eq(json)
+    end
+
   end
+
+
 end
