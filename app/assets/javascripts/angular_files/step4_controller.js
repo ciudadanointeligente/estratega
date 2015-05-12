1c373dc335ee76d3e8baa2c55139951886a6c327
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-05-12 12:05:49 -0300

    ability to edit or remove an outcome

--- app/assets/javascripts/angular_files/step4_controller.js
+++ app/assets/javascripts/angular_files/step4_controller.js
@@ -26,8 +26,25 @@ app.controller("step4Ctrl", function($scope, $http){
         $http.post("/objectives/1/outcomes", $scope.current_outcome)
 			.success(function(data){
 		      	// alertar en caso de success o error
-		      	console.log($scope.outcomes);
 		      	$scope.outcomes.push(data);
 	  		})
+	}
+
+	$scope.edit_outcome = function(outcome_id) {
+		if(!outcome_id || outcome_id !== parseInt(outcome_id))
+			return
+		$http.get('/objectives/1/outcomes/'+outcome_id+'.json')
+			.success(function(data){
+				$scope.current_outcome = data
+			})
+	}
+
+	$scope.delete_outcome = function(outcome_id) {
+		if(!outcome_id || outcome_id !== parseInt(outcome_id))
+			return
+		$http.delete('/objectives/1/outcomes/'+outcome_id+'.json')
+			.success(function(data){
+				get_outcomes();
+			})
 	}
 })
--- app/views/outcomes/show.json.jbuilder
+++ app/views/outcomes/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @outcome, :id, :title, :description, :objective_id, :created_at, :updated_at
+json.extract! @outcome, :id, :title, :description, :objective_id, :outcome_type_id, :actor_type_id, :created_at, :updated_at
