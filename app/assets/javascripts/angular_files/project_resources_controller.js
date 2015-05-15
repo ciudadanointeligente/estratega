92faa0065a5b2d2b6699d1eec35836d0ae71a4d7
Marcel Augsburger <maugsburger@gmail.com>
2015-05-15 17:55:30 -0300

    Added delete button on resource list

--- app/assets/javascripts/angular_files/project_resources_controller.js
+++ app/assets/javascripts/angular_files/project_resources_controller.js
@@ -1,10 +1,8 @@
 app.controller("projectResourcesCtrl", function($scope, $http, $location, $timeout, $aside){
     $scope.project_id = $location.path().split("/").pop();
-    // $scope.load_models = load_models;
     $scope.itemsByPage=15;
 
     var load_models = function(){
-        console.log("loading models")
         if($scope.project_id){
             $http.get('/projects/'+$scope.project_id+'.json')
             .success(function(data){
@@ -19,11 +17,16 @@ app.controller("projectResourcesCtrl", function($scope, $http, $location, $timeo
     load_models();
 
     $scope.open_form = function(resource){
-        // openAside(policy, 'left', true)
         openAside('left', true, resource);
     }
 
-    // var openAside = function(policy, position, backdrop) {
+    $scope.delete_resource = function(resource){
+      if(confirm('Are you absolutely sure you want to delete?')) {
+        $http.delete('/projects/'+$scope.project_id+'/resources/'+resource.id);
+        $scope.resources.splice($scope.resources.indexOf(resource),1);
+      }
+    }
+
     var openAside = function(position, backdrop, resource) {
       $scope.asideState = {
         open: true,
@@ -34,7 +37,6 @@ app.controller("projectResourcesCtrl", function($scope, $http, $location, $timeo
         $scope.asideState.open = false;
       }
 
-      // var problem_id = $scope.problem_id;
       var templateUrl = $scope.project.id + '/resources/' + (resource ? resource.id + "/aside" : 'aside')
       $scope.current_resource = resource;
 
@@ -45,20 +47,12 @@ app.controller("projectResourcesCtrl", function($scope, $http, $location, $timeo
         size: 'lg',
         backdrop: backdrop,
         controller: function($scope, $modalInstance, $timeout) {
-          // $scope.solution_button_txt = "Add Solution";
-          // $scope.policy = policy;
-          // $scope.problem_id = problem_id;
-          // $scope.project = 
           $scope.add_resource = function(e) {
             $modalInstance.close();
             e.stopPropagation();
             // only so reloading is executed after posting
             // this should be executed as a callback, on form success
             $timeout(load_models, 1);
-          };
-          $scope.ok = function(e) {
-            $modalInstance.close();
-            e.stopPropagation();
           };
           $scope.cancel = function(e) {
             $modalInstance.dismiss();
