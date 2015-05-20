293480c818d286015f01a5ebf4468f5431d1a6a6
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-05-20 17:06:36 -0300

    create a method

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -2,4 +2,9 @@ class Outcome < ActiveRecord::Base
   validates :title, presence: true
   belongs_to :objective
   has_and_belongs_to_many :activities
+
+  def type_list
+  	outcome_type = ['Political will', 'Public will', 'Attitudes or beliefs', 'Salience', 'Awareness', 'New champions', 'New advocates', 'Partnerships or alliances', 'Constituency or support-base growth', 'Media coverage', 'Issue reframing', 'Organizational advocacy capacity', 'Organizational visibility or issue recognition', 'Other']
+  	return outcome_type
+  end
 end
--- spec/models/outcome_spec.rb
+++ spec/models/outcome_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Outcome, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "return a non empty array" do
+  	myOutcome = Outcome.new
+  	expect(myOutcome.type_list).to be_an Array
+  	expect(myOutcome.type_list).not_to be_empty
+  end
+
+  it "return an array with valid values" do
+  	myOutcome = Outcome.new
+  	expect(myOutcome.type_list).to include 'Salience'
+  end
+
 end
