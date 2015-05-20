fac09c0c76b5a9809434253af2b4f28194c03a9f
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-05-20 17:24:27 -0300

    add method to return an actor type list

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,8 @@
 class Actor < ActiveRecord::Base
   validates :name, presence: true
+
+  def type_list
+  	actorTypeList = ['Active opponents with influence', 'Active opponents with low influence', 'Passive opponents with influence', 'Passive opponents with low influence', 'Neutrals with influence', 'Neutrals with low influence', 'Passive supporters with influence', 'Passive supporters with low influence', 'Active supporters with influence', 'Active supporters with low influence']
+  	return actorTypeList
+  end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Actor, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "return a non empty array" do
+  	myActorTypes = Actor.new
+  	expect(myActorTypes.type_list).to be_an Array
+  	expect(myActorTypes.type_list).not_to be_empty
+  end
+
+  it "return an array with valid values" do
+  	myActorTypes = Actor.new
+  	expect(myActorTypes.type_list).to include 'Passive supporters with influence'
+  end
 end
