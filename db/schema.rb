f12087fef821bdb449c75015e293fa0a3754b381
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-07-30 17:43:16 -0300

    modified activities model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724203525) do
+ActiveRecord::Schema.define(version: 20150730204026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20150724203525) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "project_id"
+    t.integer  "objective_id"
   end
 
   create_table "activities_outcomes", id: false, force: :cascade do |t|
