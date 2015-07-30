c8874e86f9ffd9a660dc3cc867903655f62f519b
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-07-30 12:12:34 -0300

    fix goals names

--- app/models/real_problem.rb
+++ app/models/real_problem.rb
@@ -3,7 +3,7 @@ class RealProblem < ActiveRecord::Base
   belongs_to :project
   has_many :policy_problems, dependent: :destroy
 
-  def to_graph_json 
+  def to_graph_json
     policy_problem_list = []
     policy_problems.each do |policy_problem|
       solution_list = []
@@ -21,11 +21,11 @@ class RealProblem < ActiveRecord::Base
         }
       policy_problem_list.append policy_problem_hash
     end
-    
+
     json = { "real_problem" => { "title" => self.title },
         "policy_problems" => policy_problem_list
       }.to_json
-    
+
     return json
   end
 
@@ -48,13 +48,13 @@ class RealProblem < ActiveRecord::Base
   end
 
   def focus_area_list
-    focus_area = ['Poverty and hunger', 'Universal primary education', 'Gender equality and empowerment of women', 'Child mortality', 'Maternal health', 'HIV/AIDS', 'malaria and other diseases', 'Environmental sustainability', 'Global partnership for development']
+    focus_area = ['Eradicate extreme poverty and hunger', 'Achieve universal primary education', 'Promote gender equality and empower women', 'Reduce child mortality', 'Improve maternal health', 'Combat HIV/AIDS, malaria and other diseases', 'Ensure environmental sustainability', 'Develop a global partnership for development']
     return focus_area
   end
-	
+
 	def solutions_associated
 		solutions = []
-		
+
 		policy_problems.each do |pp|
 			solutions << pp.id
 		end
