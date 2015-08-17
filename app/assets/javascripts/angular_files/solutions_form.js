eaedc122faa32b7ae4b70e82ccb055bbdbeeb097
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-08-17 16:05:56 -0300

    changed indentation

--- app/assets/javascripts/angular_files/solutions_form.js
+++ app/assets/javascripts/angular_files/solutions_form.js
@@ -1,62 +1,62 @@
 app.directive("solutionsForm", ["$http", "$location", function ($http, $location) {
-    return {
-        // scope: {
-        //     policy: "=",
-        //     problemId: "=", 
-        //     solutions : "@"
-        // },
-        restrict: "E",
-        templateUrl: "/solutions/aside_form",
-        controller: function($scope, $http){
-            $scope.solutions = [];
-            $scope.problem_id = $location.search().problem_id
+  return {
+    // scope: {
+    //     policy: "=",
+    //     problemId: "=",
+    //     solutions : "@"
+    // },
+    restrict: "E",
+    templateUrl: "/solutions/aside_form",
+    controller: function($scope, $http) {
+      $scope.solutions = [];
+      $scope.problem_id = $location.search().problem_id
 
-            $http.get("/real_problems/"+$scope.problem_id+"/policy_problems/"+$scope.policy.id+"/solutions.json")
-            .success(function(data){
-                $scope.solutions = data;
-            })
-            $scope.current_solution = {title: "", description: ""};
+      $http.get("/real_problems/"+$scope.problem_id+"/policy_problems/"+$scope.policy.id+"/solutions.json")
+      .success(function (data){
+          $scope.solutions = data;
+      })
+      $scope.current_solution = {title: "", description: ""};
 
-            $scope.save_or_update_solution = function(){
-                //if
-                if($scope.current_solution.id) {
-                    $http.put("/real_problems/"+$scope.problem_id+"/policy_problems/"+$scope.policy.id+"/solutions/"+$scope.current_solution.id, $scope.current_solution).
-                    success(function(data){
-                        $scope.solution_button_txt = "Add Solution";
-                    });
-                } else {
-                    $http.post("/real_problems/"+$scope.problem_id+"/policy_problems/"+$scope.policy.id+"/solutions", $scope.current_solution)
-                    .success(function(data){
-                        $scope.solutions.push(data)
-                    })
-                }
-                $scope.current_solution = {title: "", description: ""};
-            }
-
-            // solution
-            $scope.edit_solution = function(solution){
-              $scope.solution_button_txt = "Edit Solution";
-              $scope.current_solution = solution;
-            }
-
+      $scope.save_or_update_solution = function(){
+        //if
+        if($scope.current_solution.id) {
+          $http.put("/real_problems/"+$scope.problem_id+"/policy_problems/"+$scope.policy.id+"/solutions/"+$scope.current_solution.id, $scope.current_solution).
+          success(function (data) {
+            $scope.solution_button_txt = "Add Solution";
+          });
+        } else {
+          $http.post("/real_problems/"+$scope.problem_id+"/policy_problems/"+$scope.policy.id+"/solutions", $scope.current_solution)
+          .success(function (data) {
+            $scope.solutions.push(data)
+          })
         }
+        $scope.current_solution = {title: "", description: ""};
+      }
+
+      // solution
+      $scope.edit_solution = function(solution) {
+        $scope.solution_button_txt = "Edit Solution";
+        $scope.current_solution = solution;
+      }
+
     }
+  }
 }]);
 
 app.directive("solutionsList", ["$http", function($http){
     return {
-        scope: {
-            problemId: "=",
-            policy: "="
-        },
-        restrict: "E",
-        templateUrl: "/solutions/list",
-        controller: function($scope, $http){
-            $scope.solutions = [];
-            $http.get("/real_problems/"+$scope.problemId+"/policy_problems/"+$scope.policy.id+"/solutions.json")
-            .success(function(data){
-                $scope.solutions = data;
-            })
-        }
+      scope: {
+        problemId: "=",
+        policy: "="
+      },
+      restrict: "E",
+      templateUrl: "/solutions/list",
+      controller: function($scope, $http){
+        $scope.solutions = [];
+        $http.get("/real_problems/"+$scope.problemId+"/policy_problems/"+$scope.policy.id+"/solutions.json")
+        .success(function(data){
+          $scope.solutions = data;
+        })
+      }
     }
 }]);
--- app/assets/javascripts/angular_files/stage2_controller.js
+++ app/assets/javascripts/angular_files/stage2_controller.js
@@ -1,245 +1,245 @@
 app.controller("stage2Ctrl", ["$scope", "$http", "$aside", "$location", function ($scope, $http, $aside, $location) {
-	$scope.project_id = $location.path().split("/")[2];
+  $scope.project_id = $location.path().split("/")[2];
+  $scope.btn_problem = "Add";
+  $scope.problem = {};
+  $scope.objective_types = {};
 
-	$scope.btn_problem = "Add";
+  $http.get('/real_problems/focus_area.json')
+    .success(function (data) {
+      $scope.focus_areas = data;
+    })
 
-	$scope.problem = {};
+  function get_objective_types(project_id) {
+    $http.get('/projects/' + project_id + '/objectives/objective_types.json')
+      .success(function (data) {
+        $scope.objective_types = data;
+      })
+  }
 
-	$scope.objective_types = {};
+  get_objective_types($scope.project_id);
 
-	$http.get('/real_problems/focus_area.json')
-		.success(function (data) {
-			$scope.focus_areas = data;
-		})
+  function get_data_project(project_id) {
+    $http.get('/projects/' + project_id + '.json')
+      .success(function (data) {
+        $scope.project = data;
+        if(data.real_problem_id)
+          get_real_problem(data.real_problem_id);
+        else
+          create_real_problem(project_id);
+      });
+  }
 
-	function get_objective_types(project_id) {
-		$http.get('/projects/' + project_id + '/objectives/objective_types.json')
-			.success(function(data){
-				$scope.objective_types = data;
-			})
-	}
+  get_data_project($scope.project_id);
 
-	get_objective_types($scope.project_id);
+  function create_real_problem(project_id) {
+    $scope.problem = {
+      title: ".",
+      project_id: project_id
+    }
 
-	function get_data_project(project_id) {
-		$http.get('/projects/' + project_id + '.json')
-			.success(function (data) {
-				$scope.project = data;
-				if(data.real_problem_id)
-					get_real_problem(data.real_problem_id);
-				else
-					create_real_problem(project_id);
-			});
-	}
+    $http.post('/real_problems/', $scope.problem)
+      .success(function (data) {
+        get_data_project($scope.project_id);
+      })
+  }
 
-	get_data_project($scope.project_id);
+  function get_real_problem(real_problem_id) {
+    $http.get('/real_problems/' + real_problem_id + '.json')
+      .success(function (data) {
+        $scope.problem = data;
+        if( data.goal )
+          $scope.btn_problem = "Edit";
+      });
+  }
 
-	function create_real_problem(project_id) {
-		$scope.problem = {
-			title: ".",
-			project_id: project_id
-		}
+  function get_objectives(project_id) {
+    $http.get('/projects/' + project_id + '/objectives.json')
+      .success(function (data) {
+        $scope.objectives = data;
+      });
+  }
 
-		$http.post('/real_problems/', $scope.problem)
-			.success(function(data){
-				get_data_project($scope.project_id);
-			})
-	}
+  get_objectives($scope.project_id);
 
-	function get_real_problem(real_problem_id) {
-		$http.get('/real_problems/' + real_problem_id + '.json')
-			.success(function (data) {
-				$scope.problem = data;
-				if( data.goal )
-					$scope.btn_problem = "Edit";
-			});
-	}
+  function get_solutions(project_id) {
+    $scope.unrelated_solutions = 0;
 
-	function get_objectives(project_id) {
-		$http.get('/projects/' + project_id + '/objectives.json')
-			.success(function (data) {
-				$scope.objectives = data;
-			});
-	}
-	get_objectives($scope.project_id);
+    $http.get('/projects/' + project_id + '/solutions.json')
+      .success(function (data) {
+        $scope.solutions = data.map(function (solution) {
+          return {
+            id: solution.id,
+            title: solution.title,
+            description: solution.description,
+            objective_ids: solution.objective_ids
+          }
+        });
+        get_unrelated_solutions($scope.solutions);
+      });
+  };
 
-	function get_solutions(project_id) {
-		$scope.unrelated_solutions = 0;
+  function get_unrelated_solutions(data) {
+    var cnt = 0;
+    data.forEach(function(the_data){
+      if( the_data['objective_ids'].length < 1 ) {
+        cnt = cnt + 1;
+      }
+    });
+    $scope.unrelated_solutions = cnt;
+  }
 
-		$http.get('/projects/' + project_id + '/solutions.json')
-			.success(function (data) {
-				$scope.solutions = data.map(function (solution) {
-					return {
-						id: solution.id,
-						title: solution.title,
-						description: solution.description,
-						objective_ids: solution.objective_ids
-					}
-				});
-				get_unrelated_solutions($scope.solutions);
-			});
-	};
-	function get_unrelated_solutions(data) {
-		var cnt = 0;
-		data.forEach(function(the_data){
-			if( the_data['objective_ids'].length < 1 ) {
-				cnt = cnt + 1;
-			}
-		});
-		$scope.unrelated_solutions = cnt;
-	}
-	get_solutions($scope.project_id);
+  get_solutions($scope.project_id);
 
-	var save_or_update_objective = function () {
-		/*ugly hack*/
-		$scope.current_objective.objective = {};
-		$scope.current_objective.evaluation = [];
+  var save_or_update_objective = function () {
+    /*ugly hack*/
+    $scope.current_objective.objective = {};
+    $scope.current_objective.evaluation = [];
 
-		$scope.current_objective.objective = {
-			title: $scope.current_objective.title,
-			description: $scope.current_objective.description,
-			project_id: $scope.current_objective.project_id,
-			prioritized: $scope.current_objective.prioritized,
-			//
-			big_difference_score : $scope.current_objective.evaluation.big_difference_score,
-			big_difference_arguments : $scope.current_objective.evaluation.big_difference_arguments,
-			//
-			multiplying_effect_score : $scope.current_objective.evaluation.multiplying_effect_score,
-			multiplying_effect_arguments : $scope.current_objective.evaluation.multiplying_effect_arguments,
-			//
-			catalytic_score : $scope.current_objective.evaluation.catalytic_score,
-			catalytic_arguments: $scope.current_objective.evaluation.catalytic_arguments,
-			//
-			demand_score: $scope.current_objective.evaluation.demand_score,
-			demand_arguments: $scope.current_objective.evaluation.demand_arguments,
-			//
-			hooks_processes_score: $scope.current_objective.evaluation.hooks_processes_score,
-			hooks_processes_arguments: $scope.current_objective.evaluation.hooks_processes_arguments,
-			//
-			intuitive_score: $scope.current_objective.evaluation.intuitive_score,
-			intuitive_arguments: $scope.current_objective.evaluation.intuitive_arguments,
-			//
-			alignment_score: $scope.current_objective.evaluation.alignment_score,
-			alignment_arguments: $scope.current_objective.evaluation.alignment_arguments,
-			//
-			added_value_score: $scope.current_objective.evaluation.added_value_score,
-			added_value_arguments: $scope.current_objective.evaluation.added_value_arguments,
-			//
-			loss_gain_score: $scope.current_objective.evaluation.loss_gain_score,
-			loss_gain_arguments: $scope.current_objective.evaluation.loss_gain_arguments,
-			//
-			solution_ids: $scope.current_objective.solution_ids,
-			objective_type: $scope.current_objective.objective_type
-		}
+    $scope.current_objective.objective = {
+      title: $scope.current_objective.title,
+      description: $scope.current_objective.description,
+      project_id: $scope.current_objective.project_id,
+      prioritized: $scope.current_objective.prioritized,
+      //
+      big_difference_score : $scope.current_objective.evaluation.big_difference_score,
+      big_difference_arguments : $scope.current_objective.evaluation.big_difference_arguments,
+      //
+      multiplying_effect_score : $scope.current_objective.evaluation.multiplying_effect_score,
+      multiplying_effect_arguments : $scope.current_objective.evaluation.multiplying_effect_arguments,
+      //
+      catalytic_score : $scope.current_objective.evaluation.catalytic_score,
+      catalytic_arguments: $scope.current_objective.evaluation.catalytic_arguments,
+      //
+      demand_score: $scope.current_objective.evaluation.demand_score,
+      demand_arguments: $scope.current_objective.evaluation.demand_arguments,
+      //
+      hooks_processes_score: $scope.current_objective.evaluation.hooks_processes_score,
+      hooks_processes_arguments: $scope.current_objective.evaluation.hooks_processes_arguments,
+      //
+      intuitive_score: $scope.current_objective.evaluation.intuitive_score,
+      intuitive_arguments: $scope.current_objective.evaluation.intuitive_arguments,
+      //
+      alignment_score: $scope.current_objective.evaluation.alignment_score,
+      alignment_arguments: $scope.current_objective.evaluation.alignment_arguments,
+      //
+      added_value_score: $scope.current_objective.evaluation.added_value_score,
+      added_value_arguments: $scope.current_objective.evaluation.added_value_arguments,
+      //
+      loss_gain_score: $scope.current_objective.evaluation.loss_gain_score,
+      loss_gain_arguments: $scope.current_objective.evaluation.loss_gain_arguments,
+      //
+      solution_ids: $scope.current_objective.solution_ids,
+      objective_type: $scope.current_objective.objective_type
+    }
 
-		if ($scope.current_objective.id) {
-			$http.put('/projects/' + $scope.project_id + '/objectives/' + $scope.current_objective.id, $scope.current_objective)
-				.success(function (data) {
-					// alert success or error
-					get_solutions($scope.project_id);
-					get_objectives($scope.project_id);
-				})
-		} else {
-			$http.post('/projects/' + $scope.project_id + '/objectives', $scope.current_objective)
-				.success(function (data) {
-					$scope.current_objective = data;
-					$scope.objectives.push(data);
-					get_solutions($scope.project_id);
-					get_objectives($scope.project_id);
-				})
-		}
-	};
+    if ($scope.current_objective.id) {
+      $http.put('/projects/' + $scope.project_id + '/objectives/' + $scope.current_objective.id, $scope.current_objective)
+        .success(function (data) {
+          // alert success or error
+          get_solutions($scope.project_id);
+          get_objectives($scope.project_id);
+        })
+    } else {
+      $http.post('/projects/' + $scope.project_id + '/objectives', $scope.current_objective)
+        .success(function (data) {
+          $scope.current_objective = data;
+          $scope.objectives.push(data);
+          get_solutions($scope.project_id);
+          get_objectives($scope.project_id);
+        })
+    }
+  };
 
-	var save_or_update_problem = function () {
-		if ($scope.problem.goal == "")
-			return
+  var save_or_update_problem = function () {
+    if ($scope.problem.goal == "")
+      return
 
-		if ($scope.problem.id) {
-			$http.put("/real_problems/" + $scope.problem.id, $scope.problem)
-				.success(function (data) {
-					// alertar en caso de success o error
-				})
-		} else {
-			$scope.problem.project_id = $scope.project_id
-			$http.post("/real_problems", $scope.problem)
-				.success(function (data) {
-					$scope.problem = data;
-					$scope.btn_problem = "Edit";
-					$scope.problem_id = data.id;
-				})
-		}
-	};
+    if ($scope.problem.id) {
+      $http.put("/real_problems/" + $scope.problem.id, $scope.problem)
+        .success(function (data) {
+          // alertar en caso de success o error
+        })
+    } else {
+      $scope.problem.project_id = $scope.project_id
+      $http.post("/real_problems", $scope.problem)
+        .success(function (data) {
+          $scope.problem = data;
+          $scope.btn_problem = "Edit";
+          $scope.problem_id = data.id;
+        })
+    }
+  };
 
-	$scope.show_goal = function() {
-		$aside.open({
-			templateUrl: 'goal-aside.html',
-			placement: 'left',
-			size: 'lg',
-			scope: $scope,
-			controller: function ($scope, $modalInstance) {
-				$scope.save = function (e) {
-					save_or_update_problem();
-					$modalInstance.dismiss();
-					e.stopPropagation();
-				}
-				$scope.cancel = function (e) {
-					$modalInstance.dismiss();
-					e.stopPropagation();
-				};
-			}
-		})
-	};
+  $scope.show_goal = function() {
+    $aside.open({
+      templateUrl: 'goal-aside.html',
+      placement: 'left',
+      size: 'lg',
+      scope: $scope,
+      controller: function ($scope, $modalInstance) {
+        $scope.save = function (e) {
+          save_or_update_problem();
+          $modalInstance.dismiss();
+          e.stopPropagation();
+        }
+        $scope.cancel = function (e) {
+          $modalInstance.dismiss();
+          e.stopPropagation();
+        };
+      }
+    })
+  };
 
-	$scope.add_edit_objective = function (objective) {
-		if (objective) {
-			$scope.current_objective = objective;
-		} else
-			$scope.current_objective = {
-				title: "",
-				description: "",
-				project_id: $scope.project_id,
-				solution_ids: []
-			};
+  $scope.add_edit_objective = function (objective) {
+    if (objective) {
+      $scope.current_objective = objective;
+    } else
+      $scope.current_objective = {
+        title: "",
+        description: "",
+        project_id: $scope.project_id,
+        solution_ids: []
+      };
 
-		$aside.open({
-			templateUrl: 'objective-aside.html',
-			placement: 'left',
-			size: 'lg',
-			scope: $scope,
-			controller: function ($scope, $modalInstance) {
-				$scope.save = function (e) {
-					save_or_update_objective();
-					$modalInstance.dismiss();
-					e.stopPropagation();
-				}
-				$scope.cancel = function (e) {
-					$modalInstance.dismiss();
-					e.stopPropagation();
-				};
-			}
-		});
-	}
+    $aside.open({
+      templateUrl: 'objective-aside.html',
+      placement: 'left',
+      size: 'lg',
+      scope: $scope,
+      controller: function ($scope, $modalInstance) {
+        $scope.save = function (e) {
+          save_or_update_objective();
+          $modalInstance.dismiss();
+          e.stopPropagation();
+        }
+        $scope.cancel = function (e) {
+          $modalInstance.dismiss();
+          e.stopPropagation();
+        };
+      }
+    });
+  }
 
-	$scope.list_policy_solutions = function() {
-		$aside.open({
-			templateUrl: 'solutions-list-aside.html',
-			placement: 'left',
-			size: 'lg',
-			scope: $scope,
-			controller: function ($scope, $modalInstance) {
-				$scope.cancel = function (e) {
-					$modalInstance.dismiss();
-					e.stopPropagation();
-				};
-			}
-		});
-	}
+  $scope.list_policy_solutions = function() {
+    $aside.open({
+      templateUrl: 'solutions-list-aside.html',
+      placement: 'left',
+      size: 'lg',
+      scope: $scope,
+      controller: function ($scope, $modalInstance) {
+        $scope.cancel = function (e) {
+          $modalInstance.dismiss();
+          e.stopPropagation();
+        };
+      }
+    });
+  }
 
-	$scope.delete_objective = function (objective) {
-		if (confirm('Are you sure you want to delete this objective?')) {
-			$http.delete('/projects/' + $scope.project_id + '/objectives/' + objective.id);
-			$scope.objectives.splice($scope.objectives.indexOf(objective), 1);
-		}
-	}
+  $scope.delete_objective = function (objective) {
+    if (confirm('Are you sure you want to delete this objective?')) {
+      $http.delete('/projects/' + $scope.project_id + '/objectives/' + objective.id);
+      $scope.objectives.splice($scope.objectives.indexOf(objective), 1);
+    }
+  }
 
 }]);
