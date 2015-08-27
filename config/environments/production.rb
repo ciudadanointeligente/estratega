aedd8218b7efbc14768a5964ca812149e083fe6a
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-08-27 16:11:23 -0300

    remove default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #mailing
-  config.action_mailer.default_url_options = { :host => ENV['root_url'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
