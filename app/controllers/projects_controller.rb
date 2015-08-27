a3b21c53b691514bd43f30cf2e35dd7ee053f496
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-08-27 14:46:41 -0300

    added public path of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
-	before_action :set_project, only: [:show, :edit, :update, :destroy, :solutions, :stage1, :stage2, :share]
+  before_action :set_project, only: [:show, :edit, :update, :destroy, :solutions, :public, :stage1, :stage2, :share]
 
   respond_to :html, :json
 
@@ -35,6 +35,35 @@ class ProjectsController < ApplicationController
     @outcomes_size = 0
     @objectives.each do |o|
       @a_size = @a_size + o.actors.size
+      @barriers_size = @barriers_size + o.barriers.size
+      @factors_size = @factors_size + o.enabling_factors.size
+      @outcomes_size = @outcomes_size + o.outcomes.size
+    end
+
+    if !@project.real_problem.blank?
+      @real_problem = @project.real_problem
+      if !@project.real_problem.try(:policy_problems).try(:blank?)
+        @policy_problems = @project.real_problem.policy_problems
+        if !@project.real_problem.try(:get_solutions).try(:blank?)
+          @solutions = @project.real_problem.get_solutions
+        end
+      end
+    end
+
+    # @policy_problems = @project.real_problem.policy_problems || PolicyProblem.none
+    # @solutions = @project.real_problem.policy_problems.first.solutions || Solution.none
+    respond_with(@project)
+  end
+
+  def public
+    # authorize @project
+    @objectives = @project.objectives
+    @actors_size = 0
+    @barriers_size = 0
+    @factors_size = 0
+    @outcomes_size = 0
+    @objectives.each do |o|
+      @actors_size = @actors_size + o.actors.size
       @barriers_size = @barriers_size + o.barriers.size
       @factors_size = @factors_size + o.enabling_factors.size
       @outcomes_size = @outcomes_size + o.outcomes.size
