56b76ca59b5a496293bc4122aacd3f4474c9dd38
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2015-08-27 15:36:55 -0300

    fix env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #mailing
-  config.action_mailer.default_url_options = { host: ENV['MAILER_URL']}
+  config.action_mailer.default_url_options = { :host => ENV['root_url'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
