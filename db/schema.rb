12f054ac2ddbe17f91b9f8f8243c1adf3e07dfb7
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2015-09-14 17:49:52 -0300

    change type for scheduling in activity
    * from datetime to date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914193153) do
+ActiveRecord::Schema.define(version: 20150914204545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150914193153) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "objective_id"
-    t.datetime "scheduling"
+    t.date     "scheduling"
     t.boolean  "completion"
     t.string   "organizer"
     t.string   "activity_types"
