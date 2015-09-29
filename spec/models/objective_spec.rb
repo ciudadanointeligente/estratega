e472b74631e25920ec2bfc3a2c68b30c34e61400
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-09-29 16:45:26 -0300

    changed indentation

--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Objective, :type => :model do
   it "return an array with valid values" do
-  	objective = Objective.new
-  	expect(objective.objective_type_list).to include "Policy blocking"
+    objective = Objective.new
+    expect(objective.objective_type_list).to include "Policy blocking"
   end
 end
