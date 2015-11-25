ccf390a49d1130543d11e8fe774a49181d3986a7
Marcel Augsburger <maugsburger@localhost.localdomain>
2015-11-25 14:11:07 -0300

    Added spanish as default language

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module VizardRails
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
 
     # add bower stuff
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
