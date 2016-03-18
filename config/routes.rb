87649ce1a8e2d3381385da99cf2a16f498439585
jbci <jbari@votainteligente.cl>
2016-03-18 16:02:08 +0000

    added admin interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :organizations
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :organizations
   root to: 'visitors#index'
 
   resources :actors do
