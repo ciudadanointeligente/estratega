5b5f6b3e154abd06f2a36dbbe1dc947a3dcc9bac
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2016-03-21 16:54:12 -0300

    fix #115
    - add objective name
    - remove commented code

--- app/assets/javascripts/angular_files/stage4_controller.js
+++ app/assets/javascripts/angular_files/stage4_controller.js
@@ -12,6 +12,15 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
       scroll_to_top();
     });
 
+  $http.get('/projects/' + $scope.project_id + '/objectives/' + $scope.objective_id + '.json')
+    .success(function (data) {
+      $scope.objective_title = data.title;
+    })
+    .error(function (){
+      $scope.messages = { response: false, message: $attrs.errorgettingobjectives }
+      scroll_to_top();
+    });
+
   function get_outcomes(project_id, objective_id) {
     $http.get('/projects/'+project_id+'/objectives/'+objective_id+'/outcomes.json')
     .success(function (data) {
@@ -54,14 +63,7 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
         .success(function (data) {
           get_outcomes($scope.project_id, $scope.objective_id)
           $scope.current_outcome = data;
-          // console.log("outcomes before")
-          // console.log($scope.outcomes)
-          // $scope.outcomes.push(data);
-          // console.log("outcomes after")
-          // console.log($scope.outcomes)
           $scope.drag_outcomes.dropzones[0].push(data);
-          //$scope.$apply();
-          //init_dnd_list();
         })
         .error(function (){
           $scope.messages = {response: false, message: $attrs.errorcreatingoutcome }
@@ -109,13 +111,6 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
     if(confirm($attrs.confirmdeleteoutcome)) {
     $http.delete('/projects/'+$scope.project_id+'/objectives/'+$scope.objective_id+'/outcomes/'+outcome.id);
     $scope.outcomes.splice($scope.outcomes.indexOf(outcome),1);
-    console.log('delete outcome:')
-    console.log(outcome)
-    console.log('$scope.outcomes.indexOf(outcome)')
-    console.log($scope.outcomes.indexOf(outcome))
-    console.log('$scope.drag_outcomes.dropzones:')
-    console.log($scope.drag_outcomes.dropzones[0])
-    
     $scope.drag_outcomes.dropzones[0].splice($scope.drag_outcomes.dropzones[0].indexOf(outcome),1);
     }
   }
@@ -123,7 +118,7 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
   $scope.dismiss_modal = function(){
     $scope.messages = {response: "", message: ""}
   }
-    
+
   $scope.add_edit_indicator = function(outcome) {
     $scope.outcome = outcome;
     if(outcome.indicator_id) {
@@ -158,7 +153,7 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
     });
 
   }
-  
+
   var save_or_update_indicator = function() {
     if($scope.outcome.indicator_id) {
       $http.put('/outcomes/'+$scope.outcome.id+'/indicators/'+$scope.outcome.indicator_id, $scope.current_indicator)
@@ -183,14 +178,14 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
     }
 
   }
-  
+
   $scope.getInclude = function(){
     if(true){
         return "item.htm";
     }
     return "";
   }
-  
+
   function init_dnd_list() {
     if ($scope.drag_outcomes) {
           var obj = JSON.parse($scope.current_objective.theory_of_change);
@@ -215,13 +210,11 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
     $scope.$watch('drag_outcomes', function(model) {
         if ($scope.current_objective) {
           $scope.current_objective.theory_of_change = JSON.stringify($scope.drag_outcomes);
-          // console.log('$scope.current_objective.theory_of_change')
-          // console.log($scope.current_objective.theory_of_change)
           save_or_update_objective();
         }
     }, true);
   };
-  
+
   function get_objective(project_id, objective_id) {
     $http.get('/projects/' + project_id + '/objectives/' + objective_id + '.json')
       .success(function (data) {
@@ -233,16 +226,15 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
         scroll_to_top();
       });
   }
+
+
+
   get_objective($scope.project_id, $scope.objective_id);
-  
+
   var save_or_update_objective = function () {
     if ($scope.current_objective.id) {
       $http.put('/projects/' + $scope.project_id + '/objectives/' + $scope.current_objective.id, $scope.current_objective)
         .success(function (data) {
-          // console.log('saved objective')
-          // console.log(data)
-          //get_solutions($scope.project_id);
-          //get_objectives($scope.project_id);
         })
         .error(function (){
           $scope.messages = { response: false, message: $attrs.errorupdatingobjective }
@@ -252,9 +244,6 @@ app.controller("stage4Ctrl", ["$scope", "$http", "$aside", "$location", "$attrs"
       $http.post('/projects/' + $scope.project_id + '/objectives', $scope.current_objective)
         .success(function (data) {
           $scope.current_objective = data;
-          //$scope.objectives.push(data);
-          //get_solutions($scope.project_id);
-          //get_objectives($scope.project_id);
         })
         .error(function (){
           $scope.messages = { response: false, message: $attrs.errorcreatingobjective }
