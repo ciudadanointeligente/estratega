8e30c7e1b8f07ac5975bb9500f8039b280af322a
Marcel Augsburger <maugsburger@gmail.com>
2016-04-04 15:31:15 -0300

    Added client supplied locale, closes #127

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'ng-rails-csrf'
 gem 'acts-as-taggable-on'
 gem 'pundit'
 gem 'icalendar'
+gem 'http_accept_language'
 
 group :development do
   gem 'better_errors'
--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module VizardRails
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = %w(es en)
     config.i18n.default_locale = :es
 
     # add bower stuff
