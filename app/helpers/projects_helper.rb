b51ec404c7dfaf2194fab57889907d866b7aca0a
Marcel Augsburger <maugsburger@gmail.com>
2016-04-11 17:43:26 -0300

    Translated project list and project page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -22,46 +22,46 @@ module ProjectsHelper
   def title_stage stage
     case stage
     when 1
-      (@real_problem && @policy_problems && @solutions) ? "Etapa completada" : "Etapa sin completar"
+      (@real_problem && @policy_problems && @solutions) ? t('projects.show.complete_stage_tooltip') : t('projects.show.incomplete_stage_tooltip')
     when 2
-      @objectives.blank? ? "Etapa sin completar" : "Etapa completada"
-    when 3
-      ( @a_size > 0 && @factors_size > 0 && !@project.activities.nil? ) ? "Etapa completada" : "Etapa sin completar"
+      @objectives.blank? ? t('projects.show.incomplete_stage_tooltip') : t('projects.show.complete_stage_tooltip')
+    when 2
+      ( @a_size > 0 && @factors_size > 0 && !@project.activities.nil? ) ? t('projects.show.complete_stage_tooltip') : t('projects.show.incomplete_stage_tooltip')
     end
   end
 
   def link_text element_symbol
     case element_symbol
     when :real_problem
-      @real_problem.blank? ? 'Articular el problema del mundo real que se desea abordar' : (@real_problem.title != "." ? @real_problem.title : 'Articular el problema del mundo real que se desea abordar')
+      @real_problem.blank? ? t('projects.show.step1_description') : (@real_problem.title != "." ? @real_problem.title : t('projects.show.step1_description'))
     when :policy_problems
-      @policy_problems.blank? ? "Identificar los dimensiones políticas del problema" : " #{@policy_problems.size} Causas políticas"
+      @policy_problems.blank? ? t('projects.show.step2_description') : " #{@policy_problems.size} #{t('projects.show.causes')}"
     when :solutions
-      @solutions.blank? ? "Soluciones políticas: Describir las principales soluciones políticas que pudieran constituir una respuesta adecuada" : " #{@solutions.size} Soluciones"
+      @solutions.blank? ? t('projects.show.step3_description') : " #{@solutions.size} #{t('projects.show.solutions')}"
     when :objectives
-      @objectives.blank? ? 'Añada y priorice objetivos' : " #{@objectives.size} Objetivos"
+      @objectives.blank? ? t('projects.show.step4_description') : " #{@objectives.size} #{t('projects.show.objectives')}"
     end
   end
 
   def objective_link_text objective, element_symbol
     case element_symbol
     when :actors
-      objective.actors.blank? ? "Lista de Actores y Mapa de Poder" : " #{objective.actors.size} Actores"
+      objective.actors.blank? ? t('projects.show.actors_description') : " #{objective.actors.size} #{t('projects.show.actors')}"
     when :enabling_factors
-      objective.enabling_factors.blank? ? "Identificado 0 factores habilitantes y #{objective.barriers.size} barreras" : " #{objective.enabling_factors.size} Factores habilitantes and #{objective.barriers.size} Barreras"
+      objective.enabling_factors.blank? ? t('projects.show.external_factors_description') : " #{objective.enabling_factors.size} #{t('projects.show.enabling_factors')}, #{objective.barriers.size} #{t('projects.show.barriers')}"
     when :outcomes
-      objective.outcomes.blank? ? "Defina Resultados intermedios" : " #{objective.outcomes.size} Resultados Intermedios"
+      objective.outcomes.blank? ? t('projects.show.outcomes_description') : " #{objective.outcomes.size} #{t('projects.show.outcomes')}"
     when :activities
-      objective.activities.blank? ? "Defina actividades" : " #{objective.activities.size} Actividades"
+      objective.activities.blank? ? t('projects.show.activities_description') : " #{objective.activities.size} #{t('projects.show.activities')}"
     when :asks
-      objective.asks.blank? ? "Defina peticiones" : " #{objective.asks.size} Peticiones"
+      objective.asks.blank? ? t('projects.show.asks_description') : " #{objective.asks.size} #{t('projects.show.asks')}"
     end
   end
   
   def outcome_link_text outcome, element_symbol
     case element_symbol
     when :asks
-      outcome.asks.blank? ? "Defina peticiones" : " #{outcome.asks.size} Peticiones"
+      outcome.asks.blank? ? t('projects.show.asks_description') : " #{outcome.asks.size} #{t('projects.show.asks')}"
     end
   end
 
