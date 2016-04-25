ca7cafb81009be1a3906bac883968ea86de060bc
jbci <jbari@votainteligente.cl>
2016-04-25 19:53:53 +0000

    try hardcoded default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #mailing
-  config.action_mailer.default_url_options = { host: ENV['BASE_URL'] }
+  config.action_mailer.default_url_options = { host: 'ciudadanoi.org' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
